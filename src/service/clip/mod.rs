38533933344fdd534103e057b8c9184404496cf8
soundchaser128 <soundchaser128@gmail.com>
2023-05-19 19:34:45 +0200

    split up into files

--- src/service/clip/mod.rs
+++ src/service/clip/mod.rs
@@ -1,9 +1,20 @@
 use super::{Clip, Marker};
-use crate::util::create_seeded_rng;
-use rand::{rngs::StdRng, seq::SliceRandom, Rng};
+use crate::{
+    service::clip::{
+        default::{DefaultClipCreator, DefaultClipOptions},
+        pmv::{PmvClipCreator, PmvClipOptions},
+        sort::{ClipSorter, RandomClipSorter, SceneOrderClipSorter},
+    },
+    util::create_seeded_rng,
+};
+use rand::rngs::StdRng;
 use serde::Deserialize;
-use std::{collections::HashMap, fmt::Debug};
-use tracing::{debug, info};
+use std::fmt::Debug;
+use tracing::info;
+
+mod default;
+mod pmv;
+mod sort;
 
 const MIN_DURATION: f64 = 2.0;
 
@@ -110,166 +121,6 @@ pub trait ClipCreator {
     ) -> Vec<Clip>;
 }
 
-#[derive(Debug)]
-pub struct PmvClipOptions {
-    pub seed: Option<String>,
-    pub clip_duration: u32,
-    pub video_duration: f64,
-}
-
-pub struct PmvClipCreator;
-
-impl ClipCreator for PmvClipCreator {
-    type Options = PmvClipOptions;
-
-    fn create_clips(
-        &self,
-        markers: Vec<Marker>,
-        options: Self::Options,
-        rng: &mut StdRng,
-    ) -> Vec<Clip> {
-        info!(
-            "using PmvClipCreator to create clips, options: {:?}",
-            options
-        );
-        let duration = options.clip_duration as f64;
-        let clip_lengths = [
-            (duration / 2.0).max(MIN_DURATION),
-            (duration / 3.0).max(MIN_DURATION),
-            (duration / 4.0).max(MIN_DURATION),
-        ];
-
-        let max_duration = options.video_duration;
-        let mut total_duration = 0.0;
-        let mut clips = vec![];
-        let mut marker_idx = 0;
-
-        let mut start_times: HashMap<i64, (f64, usize)> =
-            markers.iter().map(|m| (m.id.inner(), (0.0, 0))).collect();
-
-        while total_duration <= max_duration {
-            let marker = &markers[marker_idx % markers.len()];
-            let clip_duration = clip_lengths.choose(rng).expect("must find one element");
-
-            let (start, index) = start_times[&marker.id.inner()];
-            let end = (start + clip_duration).min(marker.end_time);
-            let duration = end - start;
-            if duration >= MIN_DURATION {
-                debug!("adding clip {start} - {end}");
-                clips.push(Clip {
-                    index_within_marker: index,
-                    index_within_video: marker.index_within_video,
-                    marker_id: marker.id,
-                    range: (start, end),
-                    source: marker.video_id.source(),
-                    video_id: marker.video_id.clone(),
-                });
-            }
-
-            total_duration += duration;
-            marker_idx += 1;
-            start_times.insert(marker.id.inner(), (end, index + 1));
-        }
-
-        let clips_duration: f64 = clips.iter().map(|c| c.duration()).sum();
-        if clips_duration > max_duration {
-            let slack = (clips_duration - max_duration) / clips.len() as f64;
-            for clip in &mut clips {
-                clip.range.1 = clip.range.1 - slack;
-            }
-        }
-
-        clips
-    }
-}
-
-#[derive(Debug)]
-pub struct DefaultClipOptions {
-    pub clip_duration: u32,
-    pub seed: Option<String>,
-}
-
-pub struct DefaultClipCreator;
-
-impl ClipCreator for DefaultClipCreator {
-    type Options = DefaultClipOptions;
-
-    fn create_clips(
-        &self,
-        markers: Vec<Marker>,
-        options: Self::Options,
-        rng: &mut StdRng,
-    ) -> Vec<Clip> {
-        info!("using DefaultClipCreator to create clips, options: {options:#?}",);
-        let duration = options.clip_duration as f64;
-        let clip_lengths = [
-            (duration / 2.0).max(MIN_DURATION),
-            (duration / 3.0).max(MIN_DURATION),
-            (duration / 4.0).max(MIN_DURATION),
-        ];
-        let mut clips = vec![];
-        for marker in markers {
-            let start = marker.start_time;
-            let end = marker.end_time;
-
-            debug!("clip start = {start}, end = {end}");
-
-            let mut index = 0;
-            let mut offset = start;
-            while offset < end {
-                let duration = clip_lengths.choose(rng).unwrap();
-                let start = offset;
-                let end = (offset + duration).min(end);
-                let duration = end - start;
-                if duration > MIN_DURATION {
-                    debug!("adding clip {} - {}", start, end);
-                    clips.push(Clip {
-                        source: marker.video_id.source(),
-                        video_id: marker.video_id.clone(),
-                        marker_id: marker.id,
-                        range: (start, end),
-                        index_within_marker: index,
-                        index_within_video: marker.index_within_video,
-                    });
-                    index += 1;
-                }
-                offset += duration;
-            }
-        }
-
-        clips
-    }
-}
-
-pub trait ClipSorter {
-    fn sort_clips(&self, clips: Vec<Clip>, rng: &mut StdRng) -> Vec<Clip>;
-}
-
-#[derive(Debug)]
-pub struct RandomClipSorter;
-
-impl ClipSorter for RandomClipSorter {
-    fn sort_clips(&self, mut clips: Vec<Clip>, rng: &mut StdRng) -> Vec<Clip> {
-        info!("sorting clips with RandomClipSorter");
-        clips.shuffle(rng);
-        clips
-    }
-}
-
-#[derive(Debug)]
-pub struct SceneOrderClipSorter;
-
-impl ClipSorter for SceneOrderClipSorter {
-    fn sort_clips(&self, clips: Vec<Clip>, rng: &mut StdRng) -> Vec<Clip> {
-        info!("sorting clips with SceneOrderClipSorter");
-        let mut clips: Vec<_> = clips.into_iter().map(|c| (c, rng.gen::<usize>())).collect();
-        clips.sort_by_key(|(clip, random)| {
-            (clip.index_within_video, clip.index_within_marker, *random)
-        });
-        clips.into_iter().map(|(clip, _)| clip).collect()
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use assert_approx_eq::assert_approx_eq;
@@ -279,7 +130,7 @@ mod tests {
     use crate::{
         service::{
             clip::{
-                arrange_clips, ClipCreator, ClipSorter, PmvClipCreator, PmvClipOptions,
+                arrange_clips, sort::ClipSorter, ClipCreator, PmvClipCreator, PmvClipOptions,
                 SceneOrderClipSorter,
             },
             fixtures::{self, create_marker_video_id},

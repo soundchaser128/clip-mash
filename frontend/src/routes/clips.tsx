392beaea2c1910cd501fe5f91e0e5368a9ae5545
soundchaser128 <soundchaser128@gmail.com>
2023-05-27 20:06:57 +0200

    formatting fixes

--- frontend/src/routes/clips.tsx
+++ frontend/src/routes/clips.tsx
@@ -44,7 +44,7 @@ const BeatIndicator: React.FC<{offsets: number[]; autoPlay: boolean}> = ({
         setShowBeat(true)
         window.setTimeout(() => setShowBeat(false), 250)
         offsetIndex.current += 1
-        setMeasureCount(n => n + 1)
+        setMeasureCount((n) => n + 1)
       }
       const delta = time - lastTime.current
       totalTime.current += delta
@@ -61,13 +61,16 @@ const BeatIndicator: React.FC<{offsets: number[]; autoPlay: boolean}> = ({
   }, [autoPlay])
 
   return (
-    <div
-      className={clsx(
-        "w-12 h-12 self-center rounded-full my-2 flex items-center justify-center",
-        showBeat ? "bg-red-500" : "bg-white"
-      )}
-    >
-      {measureCount}
+    <div className="flex items-center justify-between pl-1">
+      <span className="label-text">Beat indicator</span>
+      <div
+        className={clsx(
+          "w-12 h-12 self-center rounded-full my-2 flex items-center justify-center",
+          showBeat ? "bg-red-500" : "bg-white"
+        )}
+      >
+        {measureCount}
+      </div>
     </div>
   )
 }
@@ -255,21 +258,19 @@ const ClipSettingsForm: React.FC<{initialValues: Inputs}> = ({
             <>
               <div className="form-control">
                 <label className="label cursor-pointer">
-                  <span className="label-text">
-                    Minimum
-                  </span>
+                  <span className="label-text">Minimum</span>
                 </label>
                 <input
                   type="number"
                   className="input input-bordered"
-                  {...register("measureCountRandomStart", {valueAsNumber: true})}
+                  {...register("measureCountRandomStart", {
+                    valueAsNumber: true,
+                  })}
                 />
               </div>
               <div className="form-control">
                 <label className="label cursor-pointer">
-                  <span className="label-text">
-                    Maximum
-                  </span>
+                  <span className="label-text">Maximum</span>
                 </label>
                 <input
                   type="number"

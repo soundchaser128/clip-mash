d7f3333f602cf8020a339e5d01290d7c9d2e410c
soundchaser128 <soundchaser128@gmail.com>
2023-06-01 07:59:25 +0200

    change paths?

--- src/service/updater.rs
+++ src/service/updater.rs
@@ -1,11 +1,11 @@
-use std::env::consts::OS;
+/// Self-update logic ported over from https://github.com/mitsuhiko/rye/blob/ecfc17e7c31137d060d43e22d93637541aa0b051/rye/src/cli/rye.rs#L131-L190
+use std::env::consts::{EXE_EXTENSION, OS};
 
 use axum::body::Bytes;
 use camino::Utf8Path;
 use color_eyre::eyre::bail;
 use tracing::info;
 
-/// Self-update logic ported over from https://github.com/mitsuhiko/rye/blob/ecfc17e7c31137d060d43e22d93637541aa0b051/rye/src/cli/rye.rs#L131-L190
 use crate::Result;
 
 const GITHUB_REPO: &str = "https://github.com/soundchaser128/clip-mash";
@@ -81,11 +81,11 @@ pub async fn self_update(tag: Option<&str>) -> Result<()> {
         format!("{GITHUB_REPO}/releases/download/{version}/{binary}")
     };
     let bytes = download_url(&url).await?;
-    let tmp = tempfile::NamedTempFile::new()?;
-    let path = Utf8Path::from_path(tmp.path()).expect("path must be utf-8");
-    unzip_file(bytes, path)?;
+    let path = Utf8Path::new("clip-mash-new").with_extension(EXE_EXTENSION);
+    unzip_file(bytes, &path)?;
 
-    info!("unzipped executable, replacing self");
-    self_replace::self_replace(tmp.path())?;
+    info!("unzipped executable to {path}, replacing self");
+    self_replace::self_replace(path)?;
+
     Ok(())
 }

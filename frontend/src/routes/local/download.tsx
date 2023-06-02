c8536666a355820ac83fedb7b16b37dd8afbca0d
soundchaser128 <soundchaser128@gmail.com>
2023-06-02 19:23:26 +0200

    improve downloads page

--- frontend/src/routes/local/download.tsx
+++ frontend/src/routes/local/download.tsx
@@ -1,11 +1,22 @@
-import {useFieldArray, useForm} from "react-hook-form"
+import {useForm} from "react-hook-form"
 import Field from "../../components/Field"
-import {HiArrowDownTray} from "react-icons/hi2"
+import {HiArrowDownTray, HiXMark} from "react-icons/hi2"
 import {useNavigate} from "react-router"
 
-type Inputs = {urls: string[]}
+type Inputs = {urls: string}
 
 type JsonError = {error: string}
+
+const splitRegex = /\s+|\n/
+
+const validateUrl = (url: string): string | undefined => {
+  try {
+    new URL(url)
+    return undefined
+  } catch (err: unknown) {
+    return (err as Error).message
+  }
+}
 
 const DownloadVideosPage: React.FC = () => {
   const navigate = useNavigate()
@@ -14,43 +25,66 @@ const DownloadVideosPage: React.FC = () => {
     register,
     formState: {errors, isSubmitting},
     setError,
-    control,
-  } = useForm<Inputs>({defaultValues: {urls: ["hello", "world"]}})
-  const {fields, append, prepend, remove, swap, move, insert} =
-    useFieldArray<Inputs>({
-      control,
-      name: "urls",
-    })
+    clearErrors,
+  } = useForm<Inputs>()
 
   const onSubmit = async (values: Inputs) => {
-    // const response = await fetch(
-    //   `/api/local/video/download?url=${encodeURIComponent(values.url)}`,
-    //   {method: "POST"}
-    // )
-    // if (response.ok) {
-    //   navigate("/local/videos")
-    // } else {
-    //   const json = (await response.json()) as JsonError
-    //   setError("url", {message: json.error})
-    // }
+    const urls = values.urls.split(splitRegex).map((res) => res.trim())
+    const errors = []
+    for (const url of urls) {
+      const validationError = validateUrl(url)
+      if (validationError) {
+        errors.push(`Invalid URL ${url}: ${validationError}`)
+      }
+    }
+    if (errors.length > 0) {
+      setError("urls", {message: errors.join("\n")})
+    } else {
+      const promises = urls.map((url) => {
+        return fetch(
+          `/api/local/video/download?url=${encodeURIComponent(url)}`,
+          {method: "POST"}
+        )
+      })
+      const responses = await Promise.all(promises)
+      const errors = []
+      for (const response of responses) {
+        if (!response.ok) {
+          const json = (await response.json()) as JsonError
+          errors.push(json.error)
+        }
+      }
+      if (errors.length > 0) {
+        setError("urls", {message: errors.join("\n")})
+      } else {
+        navigate("/local/videos")
+      }
+    }
   }
 
   return (
     <>
+      {errors.urls && (
+        <div className="bg-error p-4 shadow-xl rounded-xl text-error-content max-w-2xl self-center mb-4 relative">
+          <HiXMark
+            className="absolute top-2 right-2 w-6 h-6 cursor-pointer"
+            onClick={() => clearErrors("urls")}
+          />
+          <p className="font-bold">Some downloads failed:</p>
+          <pre>{errors.urls.message}</pre>
+        </div>
+      )}
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="max-w-lg w-full self-center flex flex-col"
       >
-        {errors.urls && <span>Failed to download: {errors.urls.message}</span>}
-
-        <Field label="Video URL">
-          {fields.map((field, index) => (
-            <input
-              key={field.id}
-              className="input input-bordered"
-              {...register(`urls.${index}` as const)}
-            />
-          ))}
+        <Field label="Video URLs">
+          <textarea
+            className="textarea textarea-bordered"
+            placeholder="Enter URLs separated by line breaks..."
+            rows={5}
+            {...register(`urls`)}
+          />
         </Field>
 
         <button

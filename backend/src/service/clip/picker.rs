f26494b0a4c484efcbb0547491711890d2e775b7
soundchaser128 <soundchaser128@gmail.com>
2023-06-04 09:26:05 +0200

    write some code

--- backend/src/service/clip/picker.rs
+++ backend/src/service/clip/picker.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 
 use clip_mash_types::Clip;
-use rand::rngs::StdRng;
+use rand::{
+    distributions::WeightedIndex, prelude::Distribution, rngs::StdRng, seq::IteratorRandom,
+};
 use tracing::info;
 
 use crate::service::Marker;
@@ -86,6 +88,7 @@ pub struct WeightedRandomClipPicker;
 pub struct WeightedRandomClipPickerOptions {
     pub weights: HashMap<String, f64>,
     pub length: f64,
+    pub clip_lengths: PmvClipLengths,
 }
 
 impl ClipPicker for WeightedRandomClipPicker {
@@ -94,12 +97,62 @@ impl ClipPicker for WeightedRandomClipPicker {
     fn pick_clips(
         &mut self,
         markers: Vec<Marker>,
-        options: Self::Options,
+        mut options: Self::Options,
         rng: &mut StdRng,
     ) -> Vec<Clip> {
-        
+        let choices: Vec<(String, f64)> = options.weights.into_iter().collect();
+        let distribution = WeightedIndex::new(choices.iter().map(|item| item.1))
+            .expect("could not build distribution");
+        let mut total_duration = 0.0;
+        let mut start_times: HashMap<i64, (f64, usize)> = markers
+            .iter()
+            .map(|m| (m.id.inner(), (m.start_time, 0)))
+            .collect();
+        let mut clips = vec![];
+
+        while total_duration <= options.length {
+            let marker_tag = &choices[distribution.sample(rng)].0;
+            let next_marker = markers
+                .iter()
+                .filter(|m| &m.title == marker_tag)
+                .choose(rng);
+            if let Some(marker) = next_marker {
+                let (start, index) = start_times[&marker.id.inner()];
+                let clip_duration = options.clip_lengths.pick_duration(rng);
+                if clip_duration.is_none() {
+                    break;
+                }
+                let clip_duration = clip_duration.unwrap();
+                let end = (start + clip_duration).min(marker.end_time);
+                let duration = end - start;
+
+                clips.push(Clip {
+                    index_within_marker: index,
+                    index_within_video: marker.index_within_video,
+                    marker_id: marker.id,
+                    range: (start, end),
+                    source: marker.video_id.source(),
+                    video_id: marker.video_id.clone(),
+                });
+
+                start_times.insert(marker.id.inner(), (end, index + 1));
+                total_duration += duration;
+            }
+        }
+
+        clips
     }
 }
 
-
 pub struct EqualLengthClipPicker;
+
+#[cfg(test)]
+mod tests {
+    use tracing_test::traced_test;
+
+    #[traced_test]
+    #[test]
+    fn test_weighted_random_clips() {
+        
+    }
+}

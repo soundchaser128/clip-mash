2aeb791a6b324137fc800d2f1a44cd3e48388988
soundchaser128 <soundchaser128@gmail.com>
2023-08-15 16:22:54 +0200

    fix compilation

--- frontend/src/components/__tests__/Modal.test.tsx
+++ frontend/src/components/__tests__/Modal.test.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import {render} from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import Modal from "../Modal"
-import {vi} from "vitest"
+import {vi, describe, it, expect} from "vitest"
 
 describe("Modal", () => {
   it("renders children when isOpen is true", () => {

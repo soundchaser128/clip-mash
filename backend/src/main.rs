c07dc317253e0c64bc1609f22287b4744b31265b
soundchaser128 <soundchaser128@gmail.com>
2023-09-09 14:13:03 +0200

    sketch out new API (wip)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -82,45 +82,58 @@ async fn main() -> Result<()> {
         ffmpeg_location,
     });
 
-    let stash_routes = Router::new()
-        .route("/health", get(handlers::stash::get_health))
-        .route("/tags", get(handlers::stash::fetch_tags))
-        .route("/performers", get(handlers::stash::fetch_performers))
-        .route("/scenes", get(handlers::stash::fetch_scenes))
-        .route("/markers", get(handlers::stash::fetch_markers))
-        .route("/config", get(handlers::stash::get_config))
-        .route("/config", post(handlers::stash::set_config));
-
-    let local_routes = Router::new()
-        .route("/video", get(handlers::local::list_videos))
-        .route("/video", post(handlers::local::add_new_videos))
-        .route("/video/:id", get(handlers::local::get_video))
-        .route("/video/:id/markers", post(handlers::local::detect_markers))
-        .route("/video/:id/file", get(handlers::local::get_video_file))
+    let library_routes = Router::new()
+        // list all videos (paginated, with search)
+        .route("/video", get(handlers::library::list_videos))
+        // add new videos either via stash, local or url
+        .route("/video", post(handlers::library::add_new_videos))
+        // get details on a single video
+        .route("/video/:id", get(handlers::library::get_video))
+        // detect markers in a video
+        .route(
+            "/video/:id/detect-markers",
+            post(handlers::library::detect_markers),
+        )
+        // stream the video file
+        .route("/video/:id/file", get(handlers::library::get_video_file))
+        // get the generated preview image
         .route(
             "/video/:id/preview",
-            get(handlers::local::get_video_preview),
+            get(handlers::library::get_video_preview),
         )
-        .route("/video/marker", get(handlers::local::list_markers))
-        .route("/video/marker", post(handlers::local::create_new_marker))
-        .route("/video/marker", put(handlers::local::update_marker))
-        .route("/video/marker/:id", delete(handlers::local::delete_marker))
+        // list all markers (paginated, with search)
+        .route("/marker", get(handlers::library::list_markers))
+        // create new marker for local video (stash tbd)
+        .route("/marker", post(handlers::library::create_new_marker))
+        // update local marker
+        .route("/marker", put(handlers::library::update_marker))
+        // delete local marker
+        .route("/marker/:id", delete(handlers::library::delete_marker))
+        // split local marker
+        .route("/marker/:id/split", post(handlers::library::split_marker));
+
+    let project_routes = Router::new()
+        .route("/clips", post(handlers::common::fetch_clips))
+        .route("/id", get(handlers::common::get_new_id))
+        .route("/create", post(handlers::common::create_video))
         .route(
-            "/video/marker/:id/split",
-            post(handlers::local::split_marker),
+            "/funscript/beat",
+            post(handlers::common::get_beat_funscript),
         )
         .route(
-            "/video/marker/:id/preview",
-            get(handlers::local::get_marker_preview),
+            "/funscript/combined",
+            post(handlers::common::get_combined_funscript),
         )
-        .route("/video/download", post(handlers::local::download_video));
+        .route(
+            "/finished-videos",
+            get(handlers::common::list_finished_videos),
+        )
+        .route("/download", get(handlers::common::download_video));
 
     let api_routes = Router::new()
-        .nest("/local", local_routes)
-        .nest("/stash", stash_routes)
+        .nest("/project", project_routes)
+        .nest("/library", library_routes)
         .route("/version", get(handlers::common::get_version))
-        .route("/clips", post(handlers::common::fetch_clips))
-        .route("/create", post(handlers::common::create_video))
         .route(
             "/progress/:id/stream",
             get(handlers::common::get_progress_stream),
@@ -129,26 +142,11 @@ async fn main() -> Result<()> {
             "/progress/:id/info",
             get(handlers::common::get_progress_info),
         )
-        .route(
-            "/finished-videos",
-            get(handlers::common::list_finished_videos),
-        )
-        .route("/download", get(handlers::common::download_video))
-        .route(
-            "/funscript/combined",
-            post(handlers::common::get_combined_funscript),
-        )
-        .route(
-            "/funscript/beat",
-            post(handlers::common::get_beat_funscript),
-        )
         .route("/song", get(handlers::common::list_songs))
         .route("/song/:id/stream", get(handlers::common::stream_song))
         .route("/song/download", post(handlers::common::download_music))
         .route("/song/upload", post(handlers::common::upload_music))
-        .route("/song/:id/beats", get(handlers::common::get_beats))
-        .route("/directory/open", get(handlers::common::open_folder))
-        .route("/id", get(handlers::common::get_new_id));
+        .route("/song/:id/beats", get(handlers::common::get_beats));
 
     let app = Router::new()
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))

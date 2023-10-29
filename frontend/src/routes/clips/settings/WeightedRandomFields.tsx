8c181fe1c1f8fdfd1f0cb62a01eadc6d5c3d5b31
soundchaser128 <soundchaser128@gmail.com>
2023-10-29 12:05:59 +0100

    fix compilation error

--- frontend/src/routes/clips/settings/WeightedRandomFields.tsx
+++ frontend/src/routes/clips/settings/WeightedRandomFields.tsx
@@ -9,24 +9,12 @@ import {MusicFormFields, RandomizedLengthFormFields} from "./common"
 const WeightedRandomFields: React.FC = () => {
   const data = useLoaderData() as ClipsLoaderData
   const {state} = useStateMachine()
-  const {register, watch} = useFormContext<ClipFormInputs>()
+  const {watch} = useFormContext<ClipFormInputs>()
   const hasSongs = state.data.songs?.length || 0 > 0
-  const useMusic = watch("roundRobin.useMusic")
+  const useMusic = watch("useMusic")
 
   return (
     <>
-      {hasSongs && (
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Use music for clip generation?</span>
-            <input
-              type="checkbox"
-              className="checkbox"
-              {...register("roundRobin.useMusic")}
-            />
-          </label>
-        </div>
-      )}
       {useMusic && hasSongs ? (
         <MusicFormFields strategy="weightedRandom" />
       ) : (

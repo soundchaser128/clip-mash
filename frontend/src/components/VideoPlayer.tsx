e15dc181efd5e718194570f276a37fabbe5e72a6
soundchaser128 <soundchaser128@gmail.com>
2023-12-08 08:07:45 +0100

    use context for player state (wip)

--- frontend/src/components/VideoPlayer.tsx
+++ frontend/src/components/VideoPlayer.tsx
@@ -1,5 +1,6 @@
 import useHotkeys from "@/hooks/useHotkeys"
 import clsx from "clsx"
+import React, {useContext, useReducer} from "react"
 import {useRef, useState} from "react"
 import {HiPause, HiPlay, HiSpeakerWave, HiSpeakerXMark} from "react-icons/hi2"
 
@@ -7,37 +8,109 @@ interface Props extends React.HTMLAttributes<HTMLVideoElement> {
   src: string
   className?: string
   wrapperClassName?: string
-  extraControls?: React.ReactNode
 }
 
-export default function VideoPlayer({
-  src,
-  className,
-  extraControls,
-  wrapperClassName,
-  ...rest
-}: Props) {
+interface PlayerState {
+  isPlaying: boolean
+  isMuted: boolean
+  currentTime: number
+  duration: number
+  playbackRate: number
+}
+
+const initialState: PlayerState = {
+  isPlaying: false,
+  isMuted: false,
+  currentTime: 0,
+  duration: 0,
+  playbackRate: 1,
+}
+
+const PlayerContext = React.createContext<{
+  state: PlayerState
+  dispatch: React.Dispatch<PlayerAction>
+}>({
+  state: initialState,
+  dispatch: () => {},
+})
+
+type PlayerAction =
+  | {type: "play"}
+  | {type: "pause"}
+  | {type: "mute"}
+  | {type: "unmute"}
+  | {type: "setDuration"; payload: number}
+  | {type: "setCurrentTime"; payload: number}
+  | {type: "setPlaybackRate"; payload: number}
+
+export function PlayerContextProvider({children}: {children: React.ReactNode}) {
+  const reducer = (state: PlayerState, action: PlayerAction) => {
+    switch (action.type) {
+      case "play":
+        return {...state, isPlaying: true}
+      case "pause":
+        return {...state, isPlaying: false}
+      case "mute":
+        return {...state, isMuted: true}
+      case "unmute":
+        return {...state, isMuted: false}
+      case "setDuration":
+        return {...state, duration: action.payload}
+      case "setCurrentTime":
+        return {...state, currentTime: action.payload}
+      case "setPlaybackRate":
+        return {...state, playbackRate: action.payload}
+      default:
+        return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <PlayerContext.Provider value={{state, dispatch}}>
+      {children}
+    </PlayerContext.Provider>
+  )
+}
+
+export function usePlayer() {
+  return useContext(PlayerContext)
+}
+
+export function Player({src, className, ...rest}: Props) {
   const videoRef = useRef<HTMLVideoElement>(null)
-  const [isMuted, setIsMuted] = useState(true)
-  const isPlaying = videoRef.current?.paused === false
+  const {dispatch} = usePlayer()
 
   const onTogglePlay = () => {
     if (videoRef.current) {
       if (videoRef.current.paused) {
         videoRef.current.play()
+        dispatch({type: "play"})
       } else {
         videoRef.current.pause()
+        dispatch({type: "pause"})
       }
     }
   }
 
   const onToggleMuted = () => {
-    setIsMuted((prev) => !prev)
+    if (videoRef.current) {
+      if (videoRef.current.muted) {
+        videoRef.current.muted = false
+        dispatch({type: "unmute"})
+      } else {
+        videoRef.current.muted = true
+        dispatch({type: "mute"})
+      }
+    }
   }
 
   const onJump = (seconds: number) => {
     if (videoRef.current) {
-      videoRef.current.currentTime += seconds
+      const newTime = videoRef.current.currentTime + seconds
+      videoRef.current.currentTime = newTime
+      dispatch({type: "setCurrentTime", payload: newTime})
     }
   }
 
@@ -49,43 +122,36 @@ export default function VideoPlayer({
   useHotkeys("left", () => onJump(-5))
 
   return (
-    <div className={wrapperClassName}>
-      <video
-        {...rest}
-        className={className}
-        src={src}
-        ref={videoRef}
-        muted={isMuted}
-      />
-      <div className="flex gap-2 items-center w-full">
-        <button
-          onClick={onTogglePlay}
-          className={clsx("btn btn-square", {
-            "btn-success": !isPlaying,
-            "btn-neutral": isPlaying,
-          })}
-          type="button"
-        >
-          {isPlaying ? (
-            <HiPause className="w-5 h-5" />
-          ) : (
-            <HiPlay className="w-5 h-5" />
-          )}
-        </button>
-        <button
-          onClick={onToggleMuted}
-          className="btn btn-square"
-          type="button"
-        >
-          {isMuted ? (
-            <HiSpeakerWave className="w-5 h-5" />
-          ) : (
-            <HiSpeakerXMark className="w-5 h-5" />
-          )}
-        </button>
+    <video {...rest} className={className} src={src} ref={videoRef} muted />
+  )
+}
 
-        {extraControls}
-      </div>
+export function PlayerControls() {
+  const {state, dispatch} = usePlayer()
+  const {isPlaying, isMuted} = state
+  return (
+    <div className="flex gap-2 items-center w-full">
+      <button
+        onClick={onTogglePlay}
+        className={clsx("btn btn-square", {
+          "btn-success": !isPlaying,
+          "btn-neutral": isPlaying,
+        })}
+        type="button"
+      >
+        {isPlaying ? (
+          <HiPause className="w-5 h-5" />
+        ) : (
+          <HiPlay className="w-5 h-5" />
+        )}
+      </button>
+      <button onClick={onToggleMuted} className="btn btn-square" type="button">
+        {isMuted ? (
+          <HiSpeakerWave className="w-5 h-5" />
+        ) : (
+          <HiSpeakerXMark className="w-5 h-5" />
+        )}
+      </button>
     </div>
   )
 }

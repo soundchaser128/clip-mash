e9554e45f9493e32a6e6d241b7231f721b490966
soundchaser128 <soundchaser128@gmail.com>
2024-04-18 09:17:13 +0200

    improve test

--- e2e-tests/tests/download.spec.ts
+++ e2e-tests/tests/download.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from "@playwright/test";
 
 const videoUrls = [
-  "https://rule34video.com/video/3073401/wattson-on-a-date-dzooworks-4k/",
-  "https://rule34video.com/video/3096413/wattson-on-holiday-break-dzooworks/",
+  // "https://rule34video.com/video/3073401/wattson-on-a-date-dzooworks-4k/",
+  // "https://rule34video.com/video/3096413/wattson-on-holiday-break-dzooworks/",
   "https://rule34video.com/video/3057656/wattson-yeero/",
   "https://rule34video.com/video/3154125/caustic-fucking-wraith-wattson-dzooworks/",
   "https://rule34video.com/video/3053449/wraith-dzooworks/",
@@ -13,13 +13,20 @@ test("download some videos", async ({ page }) => {
   await expect(page).toHaveTitle(/ClipMash/);
   await page.getByRole("link", { name: "Start" }).click();
   await page.getByRole("link", { name: "Add videos" }).click();
-  await page.getByRole("link", { name: "Folder" }).click();
   await page.getByRole("link", { name: "Download" }).click();
   await page
     .getByPlaceholder("Enter URLs separated by line")
     .fill(videoUrls.join("\n"));
   await page.getByRole("button", { name: "Download" }).click();
-  await expect(page.getByText("Downloading 5 videos...")).toBeVisible();
+  const loader = page.getByText(`Downloading ${videoUrls.length} videos...`);
+  await expect(loader).toBeVisible();
+
+  // wait until the loader disappears
+  await expect(loader).not.toBeVisible({
+    timeout: 120 * 1000,
+  });
+
+  expect(page.url()).toContain("/library");
 
   const images = await page.getByRole("img").all();
   expect(images).toHaveLength(videoUrls.length);

55cf3d0363b0a8db32544b20902e81ab495ca9cf
soundchaser128 <soundchaser128@gmail.com>
2025-06-29 17:00:02 +0200

    fix build on windows

--- crates/clip-mash-app/vite.config.ts
+++ crates/clip-mash-app/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
-import { sveltekit } from "@sveltejs/kit/vite";
+import react from "@vitejs/plugin-react";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [sveltekit()],
+  plugins: [react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
